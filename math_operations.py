74e3ffc0abb8c356a64c241ceaa92c2a489aa663
pratikshinde590 <147705816+pratikshinde590@users.noreply.github.com>
2023-12-26 18:37:05 +0530

    Create math_operations.py

---
+++ math_operations.py
@@ -0,0 +1,23 @@
+# math_operations.py
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Cannot divide by zero"
+
+# Example usage:
+# Uncomment and modify as needed for your test
+# print(add(5, 3))
+# print(subtract(8, 4))
+# print(multiply(6, 2))
+# print(divide(10, 2))
