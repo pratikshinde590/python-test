533e4f71b82324bd6cc37e3879172381f0bd2949
pratikshinde590 <147705816+pratikshinde590@users.noreply.github.com>
2023-12-27 16:51:23 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,18 +1,10 @@
 import unittest
-from add_numbers import add_numbers
+from program1 import add_numbers
+
 
 class TestAddNumbers(unittest.TestCase):
     def test_add_positive_numbers(self):
         self.assertEqual(add_numbers(2, 3), 5)
-
-    # def test_add_negative_numbers(self):
-    #     self.assertEqual(add_numbers(-2, -3), -5)
-
-    # def test_add_mixed_numbers(self):
-    #     self.assertEqual(add_numbers(2, -3), -1)
-
-    # def test_add_float_numbers(self):
-    #     self.assertAlmostEqual(add_numbers(2.5, 3.5), 6.0, places=1)
 
 if __name__ == "__main__":
     unittest.main()
