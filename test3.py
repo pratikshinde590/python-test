8d13d4b1f40ed2a20152242c191a3401c12863b1
pratikshinde590 <147705816+pratikshinde590@users.noreply.github.com>
2023-12-27 16:51:56 +0530

    Update test3.py

--- test3.py
+++ test3.py
@@ -1,1 +1,9 @@
+import unittest
+from program3 import divide_numbers
 
+class TestDivideNumbers(unittest.TestCase):
+    def test_divide_positive_numbers(self):
+        self.assertEqual(divide_numbers(6, 3), 2)
+
+if __name__ == "__main__":
+    unittest.main()
