0abd6bd365e407cbcb2201d9864eaf6aa56fafcb
Pratik Shinde <pratik.shinde@techdome.net.in>
2023-12-28 18:02:25 +0530

    Auto commit on save

--- test1.py
+++ test1.py
@@ -3,7 +3,7 @@ from program1 import add_numbers
 
 class TestMultiplyNumbers(unittest.TestCase):
     def test_add_positive_numbers(self):
-        self.assertEqual(add_numbers(2, 3), 6)
+        self.assertEqual(add_numbers(2, 3), 5)
 
 if __name__ == "__main__":
     unittest.main(argv=['first-arg-is-ignored'], exit=False)
