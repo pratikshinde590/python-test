7cd88180e3fee8058bd4a82bbe206473ee2477e4
Pratik Shinde <pratik.shinde@techdome.net.in>
2023-12-28 17:48:04 +0530

    Auto commit on save

--- test1.py
+++ test1.py
@@ -1,11 +1,12 @@
 import unittest
-from program1 import add_numbers  # Import directly from program1
+from program1 import add_numbers
 
 class TestAddNumbers(unittest.TestCase):
     def test_add_positive_numbers(self):
         self.assertEqual(add_numbers(2, 3), 5)
 
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main(module='program1')  # Specify the module explicitly
+
 
 
