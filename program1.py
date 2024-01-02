daeae16ab149ea3384dc646e6999c82d1eae33d8
Pratik Shinde <pratik.shinde@techdome.net.in>
2024-01-02 11:55:58 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -10,7 +10,5 @@ class Solution(object):
 
 
 
-
-
   
 
--- test2.py
+++ test2.py
@@ -1,13 +1,33 @@
 import unittest
-from program2 import sum_of_squares
+from program2 import Solution
 
-class TestSumOfSquares(unittest.TestCase):
-    def test_sum_of_squares(self):
-        self.assertEqual(sum_of_squares([1, 2, 3]), 14)
-        self.assertEqual(sum_of_squares([2, 4, 6]), 56)
-        self.assertEqual(sum_of_squares([5, 3, 1]), 35)
+class TestRomanToInt(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
 
-if __name__ == "__main__":
+    def test_example1(self):
+        self.assertEqual(self.solution.romanToInt("III"), 3)
+
+    def test_example2(self):
+        self.assertEqual(self.solution.romanToInt("LVIII"), 58)
+
+    def test_example3(self):
+        self.assertEqual(self.solution.romanToInt("MCMXCIV"), 1994)
+
+    def test_single_roman_digit(self):
+        self.assertEqual(self.solution.romanToInt("X"), 10)
+
+    def test_subtraction_rule(self):
+        self.assertEqual(self.solution.romanToInt("IV"), 4)
+        self.assertEqual(self.solution.romanToInt("IX"), 9)
+
+    def test_large_number(self):
+        self.assertEqual(self.solution.romanToInt("MMMCMXCIX"), 3999)
+
+    def test_empty_string(self):
+        self.assertEqual(self.solution.romanToInt(""), 0)
+
+if __name__ == '__main__':
     unittest.main(argv=['first-arg-is-ignored'], exit=False)
 
 
