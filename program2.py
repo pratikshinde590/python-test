3f11233d4f34d9fc76a6d53f344ff0d458a12b53
Pratik Shinde <pratik.shinde@techdome.net.in>
2024-01-02 11:53:07 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,9 +1,7 @@
-def sum_of_squares(numbers):
-    """
-    Takes a list of numbers and returns the sum of the squares of those numbers.
-
-    Example:
-    sum_of_squares([1, 2, 3]) => 14  # (1^2 + 2^2 + 3^2)
-    """
-    # Write your code here
-    pass
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        pass
